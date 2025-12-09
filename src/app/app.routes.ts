6a65828a3cf700bae49679427c055ecfd17469bf
enzobanin <e.banin@aluno.ifsp.edu.br>
2025-12-08 22:14:07 -0300

    feat: criando Home e alterando Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Menu } from './menu/menu/menu';
 import { Sobre } from './menu/sobre/sobre';
+import { Home } from './core/home/home';
 
 
 export const routes: Routes = [
   {path: '', redirectTo: '/menu', pathMatch: 'full' },
+  {path: 'home', component: Home},
   {path: 'menu', component: Menu},
-  {path: 'sobre', component: Sobre}
+  {path: 'sobre', component: Sobre},
 ];
