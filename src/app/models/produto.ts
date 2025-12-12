8f7896cb10fe4a1adc484fd99b1b33a952c28271
MizunoYudi <joaomizunovy@gmail.com>
2025-12-12 12:27:24 -0300

    Produto: adiconando ProdutoMap

--- src/app/models/produto.ts
+++ src/app/models/produto.ts
@@ -7,4 +7,37 @@ export interface Produto {
   preco: number;
   categoria: string;
   disponivel: boolean;
+  promo?: true,
+  desconto?: 0
 }
+
+export class ProdutoMap{
+  static deJson (json: any): Produto {
+    return {
+      id: json.id,
+      nome: json.nome,
+      url: json.url,
+      descricao: json.descricao,
+      preco: json.preco,
+      categoria: json.categoria,
+      disponivel: json.disponivel,
+      promo: json.promo,
+      desconto: json.desconto
+    }
+  }
+
+  static paraJson(p: Produto){
+    return {
+      id: p.id,
+      nome: p.nome,
+      url: p.url,
+      descricao: p.descricao,
+      preco: p.preco,
+      categoria: p.categoria,
+      disponivel: p.disponivel,
+      promo: p.promo || false,
+      desconto: p.desconto || 0
+    } 
+  }
+}
+
