e1f7f88f0196b708103611dfb65e53d49cbabc3b
MizunoYudi <joaomizunovy@gmail.com>
2025-12-12 12:30:44 -0300

    produto.service criado

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { Produto } from '../models/produto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProdutoService {
-  
+  private http = inject(HttpClient);
+  private urlAPI = "https://burgaoapi-production.up.railway.app/burgaoAPI/produtos";
+
+  listar(): Observable<Produto[]> {
+    return this.http.get<any[]>(this.urlAPI).pipe(
+      catchError(err => {
+        return of([]);
+      })
+    )
+  }
 }
